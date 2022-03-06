7ea06cf82f6439a3d90163481dc9a99d31ca72b6
mhmmdhandika <muhammadhandika78@gmail.com>
2022-03-06 15:49:30 +0700

    Add contact form to Google Sheets from Jamie Wilson and Om Prakash

---
+++ assets/script/submit-contact.js
@@ -0,0 +1,31 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxJHR66ii0F390YHGNGeAyiUwRILeScoV9AMGBV8VjGUH5k_p7C-3qI68xSF9AF6zwCLA/exec'
+const form = document.forms['submit-to-google-sheet']
+const sendButton = document.querySelector('#send-button')
+const loadingButton = document.querySelector('.btn-loading')
+const alertMessage = document.querySelector('.alert-message')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    // when submit's button is clicked
+    // show laoding button, disappear send button
+    loadingButton.classList.toggle('d-none')
+    sendButton.classList.toggle('d-none')
+
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            // show send button, disappear loading button
+            loadingButton.classList.toggle('d-none')
+            sendButton.classList.toggle('d-none')
+
+            // show alert button
+            alertMessage.classList.toggle('d-none')
+
+            // reset the form
+            form.reset()
+            console.log('Success!', response)
+        })
+        .catch(error => console.error('Error!', error.message))
+})
