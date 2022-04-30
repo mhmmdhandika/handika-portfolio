6a03b0ef6029dc355a834f35df586eea7527f72f
mhmmdhandika <muhammadhandika78@gmail.com>
2022-05-01 01:09:20 +0700

    fixed the spaces and change project Indonesia text to English

--- assets/script/submit-contact.js
+++ assets/script/submit-contact.js
@@ -5,27 +5,27 @@ const loadingButton = document.querySelector('.btn-loading')
 const alertMessage = document.querySelector('.alert-message')
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    // when submit's button is clicked
-    // show laoding button, remove send button
-    loadingButton.classList.toggle('d-none')
-    sendButton.classList.toggle('d-none')
+  e.preventDefault()
+  // when submit's button is clicked
+  // show laoding button, remove send button
+  loadingButton.classList.toggle('d-none')
+  sendButton.classList.toggle('d-none')
 
-    fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form)
-        })
-        .then(response => {
-            // show send button, remove loading button
-            loadingButton.classList.toggle('d-none')
-            sendButton.classList.toggle('d-none')
+  fetch(scriptURL, {
+      method: 'POST',
+      body: new FormData(form)
+    })
+    .then(response => {
+      // show send button, remove loading button
+      loadingButton.classList.toggle('d-none')
+      sendButton.classList.toggle('d-none')
 
-            // show alert button
-            alertMessage.classList.toggle('d-none')
+      // show alert button
+      alertMessage.classList.toggle('d-none')
 
-            // reset the form
-            form.reset()
-            console.log('Success!', response)
-        })
-        .catch(error => console.error('Error!', error.message))
+      // reset the form
+      form.reset()
+      console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
 })
